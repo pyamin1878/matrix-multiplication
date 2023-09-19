565e1a97598766065f50602c1a82af6d08e066a7
Patrick Yamin <patrickyamin@gmail.com>
2023-09-19 02:11:20 -0700

    Update comments to numpy method

--- mm_numpy.py
+++ mm_numpy.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-#input two matrices
+#Input two matrices
 matrix_1 = np.array([[1,2,3],  
                     [4,5,6],
                     [7,8,9]])
@@ -9,14 +9,8 @@ matrix_2 = np.array([[0,1,2],
                     [3,4,5],
                     [6,7,8]])
 
-result = np.dot(matrix_1, matrix_2)
-
-print(result)
-
-# return dot product
-
+# Return dot product
 res = np.dot(matrix_1,matrix_2)
 
 # print resulting matrix
-
 print(res)
