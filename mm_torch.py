ddbd5ae9ff955024f683ef1ec1a904f965e1b84c
Patrick Yamin <patrickyamin@gmail.com>
2023-09-19 02:22:34 -0700

    Update comments for numpy and pytorch

--- mm_torch.py
+++ mm_torch.py
@@ -1,6 +1,6 @@
 import torch
 
-#input two matrices
+## Define two matrices as PyTorch tensors
 
 matrix_1 = torch.tensor([[1,2,3],
                         [4,5,6],
@@ -10,10 +10,10 @@ matrix_2 = torch.tensor([[0,1,2],
                         [3,4,5],
                         [6,7,8]])
 
-# dot product with .matmul()
+# Calculate the dot product of matrix_1 and matrix_2 using .matmul()
 
 product = torch.matmul(matrix_1, matrix_2)
 
-# print resulting matrix
+# Print resulting matrix
 
 print(product)
