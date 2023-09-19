d04f91cfc25736cf580d58ce5108575d1979db3e
Patrick Yamin <patrickyamin@gmail.com>
2023-09-19 00:42:46 -0700

    Add underscores

--- mm_numpy.py
+++ mm_numpy.py
@@ -2,15 +2,15 @@
 
 import numpy as np
 
-matrix1 = np.array([[1,2,3],  
+matrix_1 = np.array([[1,2,3],  
                     [4,5,6],
                     [7,8,9]])
 
-matrix2 = np.array([[0,1,2],
+matrix_2 = np.array([[0,1,2],
                     [3,4,5],
                     [6,7,8]])
 
-result = np.dot(matrix1, matrix2)
+result = np.dot(matrix_1, matrix_2)
 
 print(result)
 
