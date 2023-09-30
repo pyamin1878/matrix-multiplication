55703769801fbd6997b8e045693d7469e1fd31f0
Patrick Yamin <patrickyamin@gmail.com>
2023-09-30 03:33:34 -0700

    Use functionswith nested for loops

--- scripts/mm.py
+++ scripts/mm.py
@@ -1,22 +1,37 @@
-# Input two matrices and label them
+def matrix_multiply_nested_loops(matrix_1, matrix_2):
+    # Get dimensions of the input matrices
+    rows_1, cols_1 = len(matrix_1), len(matrix_1[0])
+    rows_2, cols_2 = len(matrix_2), len(matrix_2[0])
 
-matrix_1 = [[1,2,3],
-            [4,5,6],
-            [7,8,9]]
-matrix_2 = [[0,1,2],
-            [3,4,5],
-            [6,7,8]]
+    # Check if the matrices can be multiplied
+    if cols_1 != rows_2:
+        return "Matrix dimensions do not match for multiplication"
 
-# Create an empty result matrix to store the multiplication result
-res = [[0 for x in range (3)] for y in range(3)]
+    # Create an empty result matrix to store the multiplication result
+    res = [[0 for x in range(cols_2)] for y in range(rows_1)]
 
-# Use nested for loops to iterate through rows and columns of the matrices
-for i in range(len(matrix_1)):
-    for j in range(len(matrix_2[0])):
-        for k in range(len(matrix_2)):
+    # Use nested for loops to iterate through rows and columns of the matrices
+    for i in range(rows_1):
+        for j in range(cols_2):
+            for k in range(rows_2):
+                # Calculate the element at row 'i' and column 'j' of the result matrix
+                res[i][j] += matrix_1[i][k] * matrix_2[k][j]
+    
+    return res
 
-            # Calculate the element at row 'i' and column 'j' of the result matrix
-            res[i][j] += matrix_1[i][k] * matrix_2[k][j]
+# Test the function
+matrix_1 = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
 
-# Print the resulting matrix
-print(res)
+matrix_2 = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8]
+]
+
+result = matrix_multiply_nested_loops(matrix_1, matrix_2)
+print("Resulting matrix:", result)
+
