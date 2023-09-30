2a825b3f2cbadf160c4299bc1784ccb85e0d97f0
Patrick Yamin <patrickyamin@gmail.com>
2023-09-30 03:47:40 -0700

    Modified comments for resulting matrix

--- scripts/mm.py
+++ scripts/mm.py
@@ -5,16 +5,16 @@ def matrix_multiply_nested_loops(matrix_1, matrix_2):
 
     # Check if the matrices can be multiplied
     if cols_1 != rows_2:
-        return "Matrix dimensions do not match for multiplication"
+        return "Matrix dimensions do not match"
 
-    # Create an empty result matrix to store the multiplication result
+    # Create an empty result matrix
     res = [[0 for x in range(cols_2)] for y in range(rows_1)]
 
     # Use nested for loops to iterate through rows and columns of the matrices
     for i in range(rows_1):
         for j in range(cols_2):
             for k in range(rows_2):
-                # Calculate the element at row 'i' and column 'j' of the result matrix
+                # Calculate the element at row 'i' and column 'j'
                 res[i][j] += matrix_1[i][k] * matrix_2[k][j]
     
     return res
